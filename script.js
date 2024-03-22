aa5880e341091d5094442990607e681c6a7f7bc8
Yashbabani0 <yashbabani09@gmail.com>
2024-03-22 19:33:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+import qr from "qr-image";
+import inquirer from "inquirer";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      /* Pass your questions in here */
+      message: "Type In Your URL",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
